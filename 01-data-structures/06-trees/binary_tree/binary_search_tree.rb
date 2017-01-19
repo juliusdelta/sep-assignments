2a347aebb439d44936a1907a9ef88e9a590e56a9
JD Gonzales <jd_gonzales@icloud.com>
2017-01-19 04:00:48 -0600

    Completed insertion method ONLY

--- 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
+++ 01-data-structures/06-trees/binary_tree/binary_search_tree.rb
@@ -1,11 +1,31 @@
 require_relative 'node'
 
 class BinarySearchTree
+  attr_accessor :root
 
   def initialize(root)
+    @root = root
   end
 
   def insert(root, node)
+    if root.rating < node.rating
+      if root.right == nil
+        root.right = node
+      elsif root.right != nil
+        root = root.right
+        self.insert(root, node)
+      end
+
+    elsif root.rating > node.rating
+      if root.left == nil
+        root.left = node
+        return
+      elsif root.left != nil
+        root = root.left
+        self.insert(root, node)
+        return
+      end
+    end
   end
 
   # Recursive Depth First Search
